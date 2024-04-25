7a37bb653c6fcbff7d0f8c4d559529208f306893
hrvdm <harvey@hvdm.io>
2024-04-25 20:30:31 +0100

    randoma11y pallette

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -2,9 +2,12 @@ import GitHub from 'assets/github.png';
 
 const Landing = () => {
   return (
-    <div className={'bg-black'}>
+    <div className={'bg-[#323d4a]'}>
       <div className="flex h-screen items-center justify-center">
-        <div className="flex flex-col items-center text-white">
+        <div className="flex flex-col items-center text-[#d9a535]">
+          <a href={'https://github.com/orgs/KickflipUX/repositories'} className='mb-6 items-center'>
+            {/* Logo placeholder for now. */}
+          </a>
           <p className="font-display font-bold mt-4 text-3xl pb-1">KICKFLIP</p>
           <p className="font-mono">( your user experience )</p>
           <div className="mt-12">
